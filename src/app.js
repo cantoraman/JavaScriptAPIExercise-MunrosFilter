53204c67aeb5ce35ef249831b52023f63302bec7
Can Toraman <cantoraman@gmail.com>
2018-08-09 11:57:02 +0100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const MunroListView = require('./views/munros_list_view.js');
+const Munros = require('./models/munros.js');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const munrosListContainer = document.querySelector('.munros-list');
+  const munrosListView = new MunroListView(munrosListContainer);
+  munrosListView.bindEvents();
+
+  const munros = new Munros();
+  munros.getData();
+
+});
---
+++ src/helpers/request.js
@@ -0,0 +1,14 @@
+const Request = function (url) {
+  this.url = url;
+};
+
+Request.prototype.get = function () {
+  return fetch(this.url)
+  .then(res => res.json());
+  //fetch is a part of dom makes a network request and returns a promise
+  //res is response from fetch that is coming from network...>>> res.json returns a promise
+};
+
+
+
+module.exports = Request;
